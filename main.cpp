6649379b022da8a6db3136010dd023997f4e2386
Viranchee Lotia <hey@viranchee.com>
2025-02-03 11:42:13 -0600

    main

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+// #include "Allocators/Allocator.h"
+#include "Allocators/AtomicAllocator.h"
+
+#define NUMTHREADS 64
+#define BLOCKSIZE 64
+#define MAXBLOCKS 16384
+
+using namespace std;
+
+int main() { return 0; }

d0a6be63122305dbe036eec66696af62695a354f
Viranchee Lotia <hey@viranchee.com>
2025-02-03 23:04:23 -0600

    begin writing tests

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 
 // #include "Allocators/Allocator.h"
 #include "Allocators/AtomicAllocator.h"
+#include <string>
+#include <thread>
+#include <vector>
 
 #define NUMTHREADS 64
 #define BLOCKSIZE 64
 #define MAXBLOCKS 16384
 
-using namespace std;
-
+// write tests
+void test1(Allocator *allocator, std::string name) {}
 int main() { return 0; }
